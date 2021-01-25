d7a04d6b6fb80e74e120a5d8eac29df126e80790
Noah Stewart <noah.stewart9813@gmail.com>
2021-01-25 16:32:00 -0500

    fix typo

--- src/config.js
+++ src/config.js
@@ -7,7 +7,7 @@ const config = {
     REGION: "us-east-1",
     URL: "https://n3347tuk74.execute-api.us-east-1.amazonaws.com/dev",
   },
-  cogntio: {
+  cognito: {
     REGION: "us-east-2",
     USER_POOL_ID: "us-east-2_e6DS2Ch0W",
     APP_CLIENT_ID: "utklo9qf1mh0rtn8ecanefjr6",
--- src/index.js
+++ src/index.js
@@ -10,15 +10,15 @@ import config from './config';
 Amplify.configure({
   Auth: {
     mandatorySignIn: true,
-    region: config.cogntio.REGION,
-    userPoolId: config.cogntio.USER_POOL_ID,
-    identityPoolId: config.cogntio.IDENTITY_POOL_ID,
-    userPoolWebClientId: config.cogntio.APP_CLIENT_ID,
+    region: config.cognito.REGION,
+    userPoolId: config.cognito.USER_POOL_ID,
+    identityPoolId: config.cognito.IDENTITY_POOL_ID,
+    userPoolWebClientId: config.cognito.APP_CLIENT_ID,
   },
   Storage: {
     region: config.s3.REGION,
     bucket: config.s3.BUCKET,
-    identityPoolId: config.cognito.IDENTITY_POOL_ID
+    identityPoolId: config.cognito.IDENTITY_POOL_ID,
   },
   API: {
     endpoints: [
